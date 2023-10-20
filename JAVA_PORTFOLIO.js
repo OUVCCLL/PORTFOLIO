e0d9d42cdba0e576ef53c872806f6ef22878a981
OUVCCLL <148568959+OUVCCLL@users.noreply.github.com>
2023-10-21 00:01:03 +0700

    Add files via upload

---
+++ JAVA_PORTFOLIO.js
@@ -0,0 +1,12 @@
+document.getElementById('scrollButton_About').addEventListener('click', function() {
+    const targetSection = document.getElementById('About');
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+
+document.getElementById('scrollButton_Experience').addEventListener('click', function() {
+    const targetSection = document.getElementById('Experience');
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+});
+
